3732786576c32d9a49d5dce99a1d3097e5c12546
Jiheui <developer.jiheuilee@gmail.com>
2024-03-12 14:56:41 +0900

    initial commit

---
+++ src/main/java/com/gdu/ioc/IOcApplication.java
@@ -0,0 +1,13 @@
+package com.gdu.ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IOcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IOcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdu/ioc/IOcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
